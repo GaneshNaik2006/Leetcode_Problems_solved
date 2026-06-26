7f48bde8eac625aa41c097114af259528d3e1de9
Kethavath Ganesh Naik <ganeshiitbombay225@gmail.com>
2026-06-26 15:50:30 +0530

    Time: 1011 ms (9.27%), Space: 469 MB (5.29%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -1,25 +1,58 @@
 class Solution {
 public:
-    long long countMajoritySubarrays(vector<int>& nums, int target) {
-        int n = nums.size();
-        // represents the occurrence count of prefix sums -n, -(n-1), ..., 0, 1,
-        // ..., n, with index offset by n.
-        vector<int> pre(n * 2 + 1, 0);
-        pre[n] = 1;
-        int cnt = n;
-        long long ans = 0, presum = 0;
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] == target) {
-                presum += pre[cnt];
-                ++cnt;
-                ++pre[cnt];
-            } else {
-                --cnt;
-                presum -= pre[cnt];
-                ++pre[cnt];
+    long long merge(vector<int>& nums,int low ,int mid,int high){
+        int left=low;
+        int right=mid+1;
+        long long cnt=0;
+        vector<int> temp;
+        while(left<=mid && right<=high){
+            if(nums[left] <nums[right]){
+                cnt+=(high-right+1);
+                temp.push_back(nums[left]);
+                left++;
+            } else{
+                temp.push_back(nums[right]);
+                right++;
             }
-            ans += presum;
         }
-        return ans;
+
+        while(left<=mid){
+            temp.push_back(nums[left]);
+            left++;
+        }
+        while(right<=high){
+             temp.push_back(nums[right]);
+             right++;
+        }
+        int k=temp.size();
+        for(int i=0;i<k;i++){
+            nums[low+i]=temp[i];
+        }
+        return cnt;
+    }
+    long long mergeSort(vector<int>& nums,int low,int high){
+        if(low>=high) return 0;
+        int mid=low+(high-low)/2;
+        long long cnt=0;
+       cnt=cnt+ mergeSort(nums,low,mid);
+       cnt= cnt+ mergeSort(nums,mid+1,high);
+       cnt=cnt+ merge(nums,low,mid,high);
+
+          return cnt;  
+    }
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==target) nums[i]=1;
+            else nums[i]=-1;
+        }
+        vector<int> ps(n+1);
+        
+        ps[0]=0;
+        for(int i=0;i<n;i++){
+            ps[i+1]=nums[i]+ps[i];
+        }
+
+       return mergeSort(ps,0,n);
     }
 };
