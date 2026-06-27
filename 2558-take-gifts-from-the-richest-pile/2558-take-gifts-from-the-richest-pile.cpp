8bc1f97d4f68b836955f1de45dfd64bda70227f2
Kethavath Ganesh Naik <ganeshiitbombay225@gmail.com>
2026-06-27 23:48:53 +0530

    Time: 0 ms (100%), Space: 13 MB (64.45%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        int n=gifts.size();
+        priority_queue<int> pq;
+        for(int i=0;i<n;i++){
+            pq.push(gifts[i]);
+        }
+
+        while(k>0 && !pq.empty()){
+            int x=pq.top();
+            pq.pop();
+            pq.push(floor(sqrt(x)));
+            k--;
+        }
+        long long sum=0;
+        while(!pq.empty()){
+            sum+=pq.top();
+            pq.pop();
+        }
+        return sum;
+    }
+};
