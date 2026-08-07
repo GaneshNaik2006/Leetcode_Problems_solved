eccb6eee48afe4a38a1b2ab2bdb7e1c7bb01b684
Kethavath Ganesh Naik <ganeshiitbombay225@gmail.com>
2026-08-07 19:33:56 +0530

    Time: 0 ms (100%), Space: 10.7 MB (47.3%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int memo(vector<int>& nums,vector<int>& dp,int i){
+        if(i==0 ) return nums[i];
+        if(i==1) return max(nums[0],nums[1]);
+        if(dp[i]!=-1) return dp[i];
+
+        int left=memo(nums,dp,i-1);
+        int include=memo(nums,dp,i-2)+nums[i];
+
+        return dp[i]=max(left,include);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+        vector<int> dp(n+1,-1);
+    
+        return memo(nums,dp,n-1);
+    }
+};
