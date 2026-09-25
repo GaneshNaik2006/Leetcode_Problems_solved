e34a1a2a51a8f05ca7cc01b99508f9fa2c1c4fcd
Ganesh Naik Kethavath <ganeshiitbombay225@gmail.com>
2026-09-25 08:46:39 +0530

    Time: 0 ms (100%), Space: 71.7 MB (51.97%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+
+        int maxi=INT_MIN;
+        int cs=0;
+
+        for(int i=0;i<n;i++){
+            cs+=nums[i];
+            maxi=max(maxi,cs);
+            if(cs<0) cs=0;
+        }
+
+        return maxi;
+    }
+};
