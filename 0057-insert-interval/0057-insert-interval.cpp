557b066972abc19d73536f7a1963cab3056b88bf
Ganesh Naik Kethavath <ganeshiitbombay225@gmail.com>
2026-09-26 21:55:46 +0530

    Time: 5 ms (9.08%), Space: 21.7 MB (75.57%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-vector<vector<int>> overlap(vector<vector<int>>& nums) {
-    int n = nums.size();
-    vector<vector<int>> ans;
-
-    ans.push_back(nums[0]);
-
-    for (int i = 1; i < n; i++) {
-        if (ans.back()[1] >= nums[i][0]) {
-            ans.back()[1] = max(ans.back()[1], nums[i][1]);
-        } else {
-            ans.push_back(nums[i]);
-        }
-    }
-
-    return ans;
-}
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-       
-
-       
-            intervals.push_back(newInterval);
-        
+        intervals.push_back(newInterval);
         sort(intervals.begin(),intervals.end());
-        return overlap(intervals);
+        int n=intervals.size();
+
+        vector<vector<int>> ans;
+
+        ans.push_back(intervals[0]);
+
+        for(int i=0;i<n;i++){
+            if(ans.back()[1]>=intervals[i][0]){
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+            }else {
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
     }
 };
